01eda3f732f805d6746b2286a3014abac1fcd1ad
Leonardo <leonardo.leonardo@accelbyte.net>
2023-11-29 09:12:34 +0700

    add: options to commit async or sync message

--- kafka.go
+++ kafka.go
@@ -578,18 +578,22 @@ func (client *KafkaClient) Register(subscribeBuilder *SubscribeBuilder) error {
 
 					return
 				}
-
 				if !client.autoCommitIntervalEnabled {
-					_, err = reader.CommitMessage(consumerMessage)
-					//todo: handle returned topic partition
-					if err != nil {
-						if subscribeBuilder.ctx.Err() == nil {
-							// the subscription is shutting down. triggered by an external context cancellation
-							loggerFields.Warn("triggered an external context cancellation. Cancelling the subscription")
-							continue
-						}
+					if subscribeBuilder.asyncCommitMessage {
+						// Asynchronously commit the offset
+						go asyncCommitMessages(reader, consumerMessage)
+					} else {
+						_, err = reader.CommitMessage(consumerMessage)
+						//todo: handle returned topic partition
+						if err != nil {
+							if subscribeBuilder.ctx.Err() == nil {
+								// the subscription is shutting down. triggered by an external context cancellation
+								loggerFields.Warn("triggered an external context cancellation. Cancelling the subscription")
+								continue
+							}
 
-						loggerFields.Error("unable to commit the event: ", err)
+							loggerFields.Error("unable to commit the event: ", err)
+						}
 					}
 				}
 			}
@@ -597,6 +601,12 @@ func (client *KafkaClient) Register(subscribeBuilder *SubscribeBuilder) error {
 	}()
 
 	return nil
+}
+
+func asyncCommitMessages(consumer *kafka.Consumer, message *kafka.Message) {
+	if _, err := consumer.CommitMessage(message); err != nil {
+		logrus.Error("unable to async commit the event: ", err)
+	}
 }
 
 // registerSubscriber add callback to map with topic and eventName as a key
